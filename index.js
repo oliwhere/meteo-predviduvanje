850a0bbe93f49cb859692aa41b7f873458d29f2e
Oliver Andonovic <oliver.andonovic@gmail.com>
2021-07-06 11:33:02 +0200

    Added user login.

---
+++ index.js
@@ -0,0 +1,73 @@
+require("dotenv").config();
+const fastify = require("fastify")({ logger: true });
+
+const firebase = require("firebase/app");
+require("firebase/auth");
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+};
+
+// Register middleware.
+fastify.register(require("fastify-sensible"));
+fastify.register(require("fastify-formbody"));
+fastify.register(require("point-of-view"), {
+  engine: {
+    pug: require("pug"),
+  },
+});
+
+// Initialize Firebase.
+firebase.initializeApp(firebaseConfig);
+
+// Gets the login page.
+fastify.get("/", async (request, reply) => {
+  reply.view("index.pug");
+});
+
+// Authenticates login details.
+fastify.post("/", async (request, reply) => {
+  const email = await request.body.email;
+  const password = await request.body.password;
+
+  if (firebase.auth().currentUser) {
+    firebase.auth().signOut();
+  } else {
+    if (password.length < 6) {
+      reply.send("Please enter a password that is at least 6 characters.");
+    }
+    // Sign in with email and pass.
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((userCreds) => {
+        reply.send("Success!");
+      })
+      .catch(function (error) {
+        // Handle errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        if (errorCode === "auth/wrong-password") {
+          reply.send("Wrong password.");
+        } else {
+          reply.send(errorMessage);
+        }
+        reply.send(error);
+      });
+  }
+});
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(3000);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
